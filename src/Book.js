eae87766861cc87e9e4a7e8e212965e7c904e445
Scott deRegt <scottderegt@gmail.com>
2018-08-02 17:47:26 -0700

    make Book and ListBooks stateless

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-class Book extends Component {
-  static propTypes={
-    bookObject: PropTypes.object.isRequired
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookObject: props.bookObject,
-    }
-  }
-  render() {
-    const imageLink = this.state.bookObject.imageLinks && this.state.bookObject.imageLinks.smallThumbnail
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLink})` }}></div>
+function Book(props) {
+  const imageLink = props.bookObject.imageLinks && props.bookObject.imageLinks.smallThumbnail
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLink})` }}></div>
 
-          <div className="book-shelf-changer">
-            <select>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+        <div className="book-shelf-changer">
+          <select>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{this.state.bookObject.title}</div>
-        <div className="book-authors">{this.state.bookObject.authors}</div>
       </div>
-    )
-  }
+      <div className="book-title">{props.bookObject.title}</div>
+      <div className="book-authors">{props.bookObject.authors}</div>
+    </div>
+  )
 }
 
 export default Book;
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,79 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Book from './Book';
 
 
-class ListBooks extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      books: props.books
-    }
-  }
-  componentWillReceiveProps(props) {
-    this.setState({books: props.books})
-  }
-  render() {
-    const read = this.state.books.filter(book => book.shelf === 'read');
-    const wantToRead = this.state.books.filter(book => book.shelf === 'wantToRead');
-    const currentlyReading = this.state.books.filter(book => book.shelf === 'currentlyReading');
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {currentlyReading.map((book) =>
-                  <li key={book.id}>
-                    <Book
-                      bookObject={book}
-                    />
-                  </li>
-                )}
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {wantToRead.map((book) =>
-                  <li key={book.id}>
-                    <Book
-                      bookObject={book}
-                    />
-                  </li>
-                )}
-              </ol>
-            </div>
-          </div>
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {read.map((book) =>
-                  <li key={book.id}>
-                    <Book
-                      bookObject={book}
-                    />
-                  </li>
-                )}
-              </ol>
-            </div>
+function ListBooks(props) {
+  const read = props.books.filter(book => book.shelf === 'read');
+  const wantToRead = props.books.filter(book => book.shelf === 'wantToRead');
+  const currentlyReading = props.books.filter(book => book.shelf === 'currentlyReading');
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Currently Reading</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {currentlyReading.map((book) =>
+                <li key={book.id}>
+                  <Book
+                    bookObject={book}
+                  />
+                </li>
+              )}
+            </ol>
           </div>
         </div>
-        <div className="open-search">
-          <Link
-            to='/search'
-          >Add a book</Link>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Want to Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {wantToRead.map((book) =>
+                <li key={book.id}>
+                  <Book
+                    bookObject={book}
+                  />
+                </li>
+              )}
+            </ol>
+          </div>
+        </div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Read</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {read.map((book) =>
+                <li key={book.id}>
+                  <Book
+                    bookObject={book}
+                  />
+                </li>
+              )}
+            </ol>
+          </div>
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link
+          to='/search'
+        >Add a book</Link>
+      </div>
+    </div>
+  )
 }
 
 export default ListBooks;
