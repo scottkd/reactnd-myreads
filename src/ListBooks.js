67d056eadd74df923baa4c0493bbad6c07a5e018
Scott deRegt <scottderegt@gmail.com>
2018-08-06 22:42:38 -0700

    ListBooks uses bookshelves list in state

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,40 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Bookshelf from './Bookshelf';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-function ListBooks(props) {
-  const read = props.books.filter(book => book.shelf === 'read');
-  const wantToRead = props.books.filter(book => book.shelf === 'wantToRead');
-  const currentlyReading = props.books.filter(book => book.shelf === 'currentlyReading');
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
+import Bookshelf from './Bookshelf'
+
+class ListBooks extends Component {
+  state = {
+    bookshelves: []
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      bookshelves: [
+        {
+          title: `Currently Reading`,
+          books: nextProps.books.filter(book => book.shelf === 'currentlyReading'),
+        },
+        {
+          title: `Want to Read`,
+          books: nextProps.books.filter(book => book.shelf === 'wantToRead'),
+        },
+        {
+          title: `Read`,
+          books: nextProps.books.filter(book => book.shelf === 'read'),
+        }
+      ]
+    })
+  }
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          {this.state.bookshelves.map((bookshelf) =>
+            <Bookshelf key={bookshelf.title}
+              shelfTitle={bookshelf.title}
+              books={bookshelf.books}
+              moveBook={this.props.moveBook}
+            />
+          )}
+        </div>
+        <div className="open-search">
+          <Link
+            to='/search'
+          >Add a book</Link>
+        </div>
       </div>
-      <div className="list-books-content">
-        <Bookshelf
-          shelfTitle='Currently Reading'
-          books={currentlyReading}
-          moveBook={props.moveBook}
-        />
-        <Bookshelf
-          shelfTitle='Want to Read'
-          books={wantToRead}
-          moveBook={props.moveBook}
-        />
-        <Bookshelf
-          shelfTitle='Read'
-          books={read}
-          moveBook={props.moveBook}
-        />
-      </div>
-      <div className="open-search">
-        <Link
-          to='/search'
-        >Add a book</Link>
-      </div>
-    </div>
-  )
+    )}
 }
 
 export default ListBooks;
